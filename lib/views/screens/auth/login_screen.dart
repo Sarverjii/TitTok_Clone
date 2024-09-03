c632a67d578b4122742d3d32ceb920f37f40e25d
Shashank Sarth Verma <sarverjii@gmail.com>
2024-09-03 06:56:50 +0530

    Modified File Structure

---
+++ lib/views/screens/auth/login_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok/constants.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      alignment: Alignment.center,
+      child: Column(
+        children: [
+          Text(
+            "Tiktok Clone",
+            style: TextStyle(
+                fontSize: 35, color: buttonColor, fontWeight: FontWeight.w900),
+          ),
+          const Text("Login",
+              style: TextStyle(fontSize: 35, fontWeight: FontWeight.w700))
+        ],
+      ),
+    ));
+  }
+}
